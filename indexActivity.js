dbe0971be4c0337b3b7a1b97d0f5a737a4df44aa
Rolen Zipagan <zru0946@dlsud.edu.ph>
2023-03-20 17:12:23 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,25 @@
+console.log(`Hello, Welcome to De La Salle University!`);
+
+
+// variables
+let fName = 'Rolen Adolf';
+let lName = 'Zipagan';
+let age = 20;
+let hobbies = ['videogames','music','movies','video editing','social media'];
+let address = {
+	houseNumber: 'Blk 7 Lot 17',
+	street: null,
+	subdi: null,
+	barangay: 'San Gabriel',
+	city: 'General Mariano Alvarez',
+	province: 'Cavite',
+	zipcode: 4117,
+};
+
+
+// console logs
+console.log(fName);
+console.log(lName);
+console.log(age);
+console.log(hobbies);
+console.log(address);
